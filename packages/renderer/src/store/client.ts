e4940705adf6cd198081c4747b0a1c6f252f6ebf
Daniel Edler <edler.dev@gmail.com>
2022-08-22 01:26:46 +0200

    Workaround to fix white screen in production

--- packages/renderer/src/store/client.ts
+++ packages/renderer/src/store/client.ts
@@ -1,10 +1,16 @@
 import io from "socket.io-client";
-// @ts-ignore
-import feathers from "@feathersjs/client";
-import socketio from "@feathersjs/socketio-client";
+// import { feathers } from "@feathersjs/feathers";
+// import socketio from "@feathersjs/socketio-client";
 
+// TODO: Use @feathersjs modules https://dove.feathersjs.com/api/client.html#others and remove script in index.html
 const socket = io("http://localhost:3030");
+
+// @ts-ignore
+declare const feathers;
+// @ts-ignore
 const client = feathers();
-client.configure(socketio(socket));
+// @ts-ignore
+client.configure(feathers.socketio(socket));
+// client.configure(socketio(socket));
 
 export default client;
