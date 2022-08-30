273601db5d974aa7178bd2e27bb6fa556780ad69
Daniel Edler <edler.dev@gmail.com>
2022-08-30 10:49:31 +0200

    build: Fix conda env name in build-python

--- packages/main/src/api.ts
+++ packages/main/src/api.ts
@@ -8,12 +8,35 @@ import type { Message } from "/shared/types";
 import fs from 'fs/promises';
 import { isDevelopment } from './utils';
 import log from "electron-log";
+import { type Application } from "./server/declarations";
 
 log.info("env:", process.env);
 
 log.info(".......................................");
 const pythonPath = join(app.getAppPath(), "python");
 log.info(`API created! Python path: '${pythonPath}'`);
+
+async function getDebugDirs() {
+  try {
+    const pythonFiles = await fs.readdir(pythonPath);
+    // const cwdFiles = await fs.readdir("."); // Root (/) in production
+    const dirContent = {
+      pythonPath,
+      pythonFiles,
+      // cwd: process.cwd(),
+      // cwdFiles
+    }
+    // log.info(dirContent);
+    return dirContent;
+  } catch (error) {
+    log.error("Error dirs:", error);
+    return {
+      pythonPath,
+      // cwd: process.cwd(),
+      error,
+    };
+  }
+}
 
 process.on("unhandledRejection", (reason, p) => {
   logger.error("Unhandled Rejection at: Promise ", p, reason);
@@ -34,46 +57,51 @@ feathersApp.listen(port).then((server) => {
 feathersApp.service("messages").on("created", async (message: Message) => {
   log.info("Api got message:", message);
 
-  try {
-    const pythonFiles = await fs.readdir(pythonPath);
-    const cwdFiles = await fs.readdir(".");
-    const dirContent = {
-      pythonPath,
-      pythonFiles,
-      cwd: process.cwd(),
-      cwdFiles
-    }
-    // log.info(dirContent);
-
-    // feathersApp.service("progress").create({
-    //   type: "stdout",
-    //   status: "progress",
-    //   data: JSON.stringify(dirContent, null, 2),
-    // });
-  } catch (error) {
-    log.error("Error dirs:", error);
-  }
+  const dirContent = await getDebugDirs();
+  feathersApp.service("progress").create({
+    type: "stdout",
+    status: "progress",
+    data: JSON.stringify(dirContent, null, 2),
+  });
 });
 
 export class PythonClient {
-  proc: ChildProcessWithoutNullStreams;
+  app: Application;
+  proc?: ChildProcessWithoutNullStreams;
 
-  constructor(native = false) {
-    if (!native) {
-      log.info(`Spawn python client.py...`);
-      this.proc = spawn("python", ["client.py"], {
-        cwd: pythonPath,
-      });
-    } else {
-      log.info(`Spawn native python client...`);
-      this.proc = spawn("client", [], {
-        cwd: pythonPath,
-      });
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  async init({ native = false }: { native?: boolean }) {
+    log.info(`Init ${native ? 'native' : 'dev'} python client...`);
+    const dirContent = await getDebugDirs();
+    log.info(`Dir content: \n${JSON.stringify(dirContent, null, 2)}`);
+
+    try {
+      if (!native) {
+        log.info(`Spawn python client.py...`);
+        this.proc = spawn("python", ["client.py"], {
+          cwd: pythonPath,
+        });
+      } else {
+        log.info(`Spawn native python client...`);
+        this.proc = spawn("client/client", [], {
+          cwd: pythonPath,
+        });
+      }
+    }
+    catch (err) {
+      log.error("Error spawning captain process", err);
+    }
+
+    if (!this.proc) {
+      return;
     }
 
     this.proc.stdout.on("data", (data) => {
       // log.info(`python stdout: ${data.toString()}`);
-      feathersApp
+      this.app
         .service("progress")
         .create({ type: "stdout", status: "progress", data: data.toString() });
     });
@@ -81,8 +109,8 @@ export class PythonClient {
     const stderrChunks: any = [];
     this.proc.stderr.on("data", (data) => {
       stderrChunks.push(data);
-      console.error(`python error: ${Buffer.concat([data]).toString()}`);
-      // feathersApp
+      log.error(`python error: ${Buffer.concat([data]).toString()}`);
+      // this.app
       //   .service("progress")
       //   .create({ type: "python-stderr", data: data.toString() });
     });
@@ -90,11 +118,13 @@ export class PythonClient {
 
   kill() {
     log.info("Killing python client...");
-    this.proc.kill();
+    this.proc?.kill();
   }
 }
 
-const pythonClient = new PythonClient(!isDevelopment);
+const pythonClient = new PythonClient(feathersApp);
+
+pythonClient.init({ native: !isDevelopment })
 
 export default {
   dispose() {
